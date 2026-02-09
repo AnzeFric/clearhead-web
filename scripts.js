0f54edae2c106aa2e2bc34b4a8d33907f3020279
AnzeFric <anzeifric@gmail.com>
2026-02-09 21:25:09 +0100

    Updated scripts.js, added menu icon class adding/removing. Added close icon

--- scripts.js
+++ scripts.js
@@ -1,4 +1,22 @@
 document.getElementById("menu-icon").addEventListener("click", function () {
   const nav = document.getElementById("expandable-nav");
+  const menuIcon = document.getElementById("menu-icon");
+
   nav.classList.toggle("hidden");
+
+  menuIcon.classList.add("icon-change");
+
+  if (nav.classList.contains("hidden")) {
+    menuIcon.src = "assets/icons/menu-icon.svg";
+  } else {
+    menuIcon.src = "assets/icons/close-icon.svg";
+  }
+
+  menuIcon.addEventListener(
+    "animationend",
+    function () {
+      menuIcon.classList.remove("icon-change");
+    },
+    { once: true }, // Removes the listener after one fire
+  );
 });
